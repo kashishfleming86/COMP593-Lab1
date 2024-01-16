ced0e4e4f76b2e3524c49b6dc9a5c8697296f420
kashishfleming86 <156828427+kashishfleming86@users.noreply.github.com>
2024-01-16 14:25:17 -0500

    modified both files in a repo clone
    deleted a print() statement from script1.py
    added two print() statements to script2.py

--- script1.py
+++ script1.py
@@ -1,2 +1,1 @@
 #print lines from zen of python
-print("pyhton is great coding language")
--- script2.py
+++ script2.py
@@ -1,2 +1,4 @@
 #print lines from zen of python
 print("my name is kashish")
+print("nice weather")
+print("hii everyone")
